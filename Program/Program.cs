769bffeef87f5fabc50f707c16e6d407bf31dcbd
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-08-13 19:44:13 -0400

    intermediate revision - added cleanup PreBuildEvent and fixed   Console_CancelKeyPress handler

--- Program/Program.cs
+++ Program/Program.cs
@@ -15,55 +15,53 @@ using OpenQA.Selenium.Support.UI;
 
 namespace WebTester
 {
-    [TestClass]
-    // [DeploymentItem(@"x86\SQLite.Interop.dll", "x86")] 
-    public class Monitor
-    {
-        private static string hub_url = "http://localhost:4444/wd/hub";
-        private static IWebDriver selenium_driver;
-        private static string baseURL = "http://www.carnival.com/";
-        private static string[] expected_states = { "interactive", "complete" };
-        // unused
-        // private static int max_cnt = 10;
-        private static string tableName = "";
-        private static string dataFolderPath;
-        private static string database;
-        private static string dataSource;
-        private static Object forever;
-        private static Boolean waiting = false;
-        private static Boolean useRemoteDriver = false;
-        private static Boolean useHeadlessDriver = true;
+	[TestClass]
+	// [DeploymentItem(@"x86\SQLite.Interop.dll", "x86")] 
+    public class Monitor {
+		private static string hub_url = "http://localhost:4444/wd/hub";
+		private static IWebDriver selenium_driver;
+		private static string baseURL = "http://www.carnival.com/";
+		private static string[] expected_states = { "interactive", "complete" };
+		// unused
+		// private static int max_cnt = 10;
+		private static string tableName = "";
+		private static string dataFolderPath;
+		private static string database;
+		private static string dataSource;
+		private static Object forever;
+		private static Boolean waiting = false;
+		private static Boolean useRemoteDriver = false;
+		private static Boolean useHeadlessDriver = true;
 
-        [TestInitialize]
-        public void Initialize()
-        {
-            dataFolderPath = Directory.GetCurrentDirectory();
-            database = String.Format("{0}\\data.db", dataFolderPath);
-            dataSource = "data source=" + database;
-            tableName = "product";
-            createTable();
-            if (useRemoteDriver) {
-                selenium_driver = new RemoteWebDriver(new Uri(hub_url), DesiredCapabilities.Chrome());
-            } else {
-                if (useHeadlessDriver) {
-                    var chromeOptions = new ChromeOptions();
-                    chromeOptions.AddArgument("--headless");
-                    selenium_driver = new ChromeDriver(chromeOptions);
-                } else {
-                    selenium_driver = new ChromeDriver();
-                }
-            }
-            selenium_driver.Manage().Timeouts().ImplicitWait = new System.TimeSpan(0, 0, 30);
-        }
+		[TestInitialize]
+		public void Initialize() {
+			dataFolderPath = Directory.GetCurrentDirectory();
+			database = String.Format("{0}\\data.db", dataFolderPath);
+			dataSource = "data source=" + database;
+			tableName = "product";
+			createTable();
+			if (useRemoteDriver) {
+				selenium_driver = new RemoteWebDriver(new Uri(hub_url), DesiredCapabilities.Chrome());
+			} else {
+				if (useHeadlessDriver) {
+					var chromeOptions = new ChromeOptions();
+					chromeOptions.AddArgument("--headless");
+					selenium_driver = new ChromeDriver(chromeOptions);
+				} else {
+					selenium_driver = new ChromeDriver();
+				}
+			}
+			selenium_driver.Manage().Timeouts().ImplicitWait = new System.TimeSpan(0, 0, 30);
+		}
 
-        [TestCleanup]
-        public void Cleanup() {
-            if (selenium_driver != null)
-                selenium_driver.Close();
-        }
+		[TestCleanup]
+		public void Cleanup() {
+			if (selenium_driver != null)
+				selenium_driver.Close();
+		}
 
-        [TestMethod]
-        // [ExpectedException(typeof(NoSuchElementException))]
+		[TestMethod]
+		// [ExpectedException(typeof(NoSuchElementException))]
         public void Sample() {
 			selenium_driver.Navigate().GoToUrl(baseURL);
 			// selenium_driver.WaitDocumentReadyState(expected_states[0]);
@@ -91,116 +89,126 @@ namespace WebTester
 				Console.Error.WriteLine("");
 			}
 		}
-        public static void Main(string[] args) {
-            dataFolderPath = Directory.GetCurrentDirectory();
-            database = String.Format("{0}\\data.db", dataFolderPath);
-            dataSource = "data source=" + database;
-            tableName = "product";
+		public static void Main(string[] args) {
+			dataFolderPath = Directory.GetCurrentDirectory();
+			database = String.Format("{0}\\data.db", dataFolderPath);
+			dataSource = "data source=" + database;
+			tableName = "product";
+			waiting = true;
+			Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
 
-            Console.WriteLine("Starting..");
-            // TestConnection();
-            createTable();
-            if (useRemoteDriver) {
-                selenium_driver = new RemoteWebDriver(new Uri(hub_url), DesiredCapabilities.Chrome());
-            } else  {
-                if (useHeadlessDriver)  {
-                    var chromeOptions = new ChromeOptions();
-                    chromeOptions.AddArgument("--headless");
-                    selenium_driver = new ChromeDriver(chromeOptions);
-                } else {
-                    selenium_driver = new ChromeDriver();
-                }
-            }
-            selenium_driver.Manage().Timeouts().ImplicitWait = new System.TimeSpan(0, 0, 30);
+			Console.WriteLine("Starting..");
+			// TestConnection();
+			createTable();
+			if (useRemoteDriver) {
+				selenium_driver = new RemoteWebDriver(new Uri(hub_url), DesiredCapabilities.Chrome());
+			} else {
+				if (useHeadlessDriver) {
+					var chromeOptions = new ChromeOptions();
+					chromeOptions.AddArgument("--headless");
+					selenium_driver = new ChromeDriver(chromeOptions);
+				} else {
+					selenium_driver = new ChromeDriver();
+				}
+			}
+			selenium_driver.Manage().Timeouts().ImplicitWait = new System.TimeSpan(0, 0, 30);
 
-            selenium_driver.Navigate().GoToUrl(baseURL);
-            // selenium_driver.WaitJqueryInActive();
-            // selenium_driver.WaitDocumentReadyState(expected_states[0]);
-            selenium_driver.WaitDocumentReadyState(expected_states);
+			selenium_driver.Navigate().GoToUrl(baseURL);
+			// selenium_driver.WaitJqueryInActive();
+			// selenium_driver.WaitDocumentReadyState(expected_states[0]);
+			selenium_driver.WaitDocumentReadyState(expected_states);
 
-            List<Dictionary<String, String>> result = selenium_driver.Performance();
-            var dic = new Dictionary<string, object>();
+			List<Dictionary<String, String>> result = selenium_driver.Performance();
+			var dic = new Dictionary<string, object>();
 
-            foreach (var row in result) {
-                dic["caption"] = "dummy";
-                foreach (string key in row.Keys) {
-                    if (Regex.IsMatch(key, "(name|duration)")) {
-                        Console.Error.WriteLine(key + " " + row[key]);
-                        if (key.IndexOf("duration") > -1) {
-                            dic[key] = (Double)Double.Parse(row[key]);
-                        } else {
-                            dic[key] = (String)row[key];
-                        }
-                    }
-                }
-                insert(dic);
+			foreach (var row in result) {
+				dic["caption"] = "dummy";
+				foreach (string key in row.Keys) {
+					if (Regex.IsMatch(key, "(name|duration)")) {
+						Console.Error.WriteLine(key + " " + row[key]);
+						if (key.IndexOf("duration") > -1) {
+							dic[key] = (Double)Double.Parse(row[key]);
+						} else {
+							dic[key] = (String)row[key];
+						}
+					}
+				}
+				insert(dic);
 
-                foreach (string key in dic.Keys.ToArray()) {
-                    dic[key] = null;
-                }
-                Console.Error.WriteLine("");
-            }
+				foreach (string key in dic.Keys.ToArray()) {
+					dic[key] = null;
+				}
+				Console.Error.WriteLine("");
+			}
 
-            Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
+			Console.Error.WriteLine("Press <CTRL-C> to quit program");
 
-            forever = new Object();
-            lock (forever) {
-                waiting = true;
-            }
-            while (waiting) {
-                // lock (forever)
-                //{
-                //    System.Threading.Monitor.Wait(forever);
-                //}
-            }
-            if (selenium_driver != null)
-                selenium_driver.Close();
-        }
+			// forever = new Object();
+			// lock (forever) {
+			// waiting = true;
+			// }
+			while (waiting) {
+				// lock (forever)
+				//{
+				//    System.Threading.Monitor.Wait(forever);
+				//}
+			}
+			Console.WriteLine("Stopped.");
 
-        public static bool insert(Dictionary<string, object> dic)
-        {
-            try {
-                using (SQLiteConnection conn = new SQLiteConnection(dataSource)) {
-                    using (SQLiteCommand cmd = new SQLiteCommand()) {
-                        cmd.Connection = conn;
-                        conn.Open();
-                        SQLiteHelper sh = new SQLiteHelper(cmd);
-                        sh.Insert(tableName, dic);
-                        conn.Close();
-                        return true;
-                    }
-                }
-            }
-            catch (Exception ex) {
-                Console.Error.WriteLine(ex.ToString());
-                return false;
-            }
-        }
+			if (selenium_driver != null)
+				try {
+					selenium_driver.Close();
+				} catch (WebDriverException) {
+					// ignore
+				}
+			selenium_driver.Quit();
+		}
 
-        public static void createTable() {
-            using (SQLiteConnection conn = new SQLiteConnection(dataSource)) {
-                using (SQLiteCommand cmd = new SQLiteCommand()) {
-                    cmd.Connection = conn;
-                    conn.Open();
-                    SQLiteHelper sh = new SQLiteHelper(cmd);
-                    sh.DropTable(tableName);
+		public static bool insert(Dictionary<string, object> dic)
+		{
+			try {
+				using (SQLiteConnection conn = new SQLiteConnection(dataSource)) {
+					using (SQLiteCommand cmd = new SQLiteCommand()) {
+						cmd.Connection = conn;
+						conn.Open();
+						SQLiteHelper sh = new SQLiteHelper(cmd);
+						sh.Insert(tableName, dic);
+						conn.Close();
+						return true;
+					}
+				}
+			} catch (Exception ex) {
+				Console.Error.WriteLine(ex.ToString());
+				return false;
+			}
+		}
 
-                    SQLiteTable tb = new SQLiteTable(tableName);
-                    tb.Columns.Add(new SQLiteColumn("id", true)); // auto increment 
-                    tb.Columns.Add(new SQLiteColumn("caption"));
-                    tb.Columns.Add(new SQLiteColumn("name"));
-                    tb.Columns.Add(new SQLiteColumn("duration", ColType.Decimal));
-                    sh.CreateTable(tb);
-                    conn.Close();
-                }
-            }
-        }
-        static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e) {
-            Console.WriteLine("Stop.");
-            System.Threading.Thread.Sleep(1);
-            waiting = false;
-            // System.Threading.Monitor.Exit(forever);
-        }
+		public static void createTable() {
+			using (SQLiteConnection conn = new SQLiteConnection(dataSource)) {
+				using (SQLiteCommand cmd = new SQLiteCommand()) {
+					cmd.Connection = conn;
+					conn.Open();
+					SQLiteHelper sh = new SQLiteHelper(cmd);
+					sh.DropTable(tableName);
 
-    }
+					SQLiteTable tb = new SQLiteTable(tableName);
+					tb.Columns.Add(new SQLiteColumn("id", true)); // auto increment 
+					tb.Columns.Add(new SQLiteColumn("caption"));
+					tb.Columns.Add(new SQLiteColumn("name"));
+					tb.Columns.Add(new SQLiteColumn("duration", ColType.Decimal));
+					sh.CreateTable(tb);
+					conn.Close();
+				}
+			}
+		}
+		// https://stackoverflow.com/questions/203246/how-can-i-keep-a-console-open-until-cancelkeypress-event-is-fired
+		// https://msdn.microsoft.com/en-us/library/system.console.cancelkeypress.aspx
+		static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e) {
+			Console.WriteLine("Stopping");
+			System.Threading.Thread.Sleep(1);
+			e.Cancel = true;
+			waiting = false;
+			// System.Threading.Monitor.Exit(forever);
+		}
+	}
 }
